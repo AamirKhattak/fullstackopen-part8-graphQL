7e3cf58a6eda4c6c6833277dd3f4028b168accbf
mrgreen_pk <aamir.fui@gmail.com>
2022-04-17 11:55:59 +0500

    8.11 COMPLETED

--- library-frontend/src/components/Authors.js
+++ library-frontend/src/components/Authors.js
@@ -1,13 +1,28 @@
-import { gql, useQuery } from "@apollo/client";
-import { ALL_AUTHORS } from "../queries.js";
+import { gql, useMutation, useQuery } from "@apollo/client";
+import { ALL_AUTHORS, EDIT_AUTHOR } from "../queries.js";
 
 const Authors = (props) => {
   const result = useQuery(ALL_AUTHORS);
+  const [editAuthor] = useMutation(EDIT_AUTHOR, {
+    onError: (error) => console.error(error),
+    refetchQueries: [{ query: ALL_AUTHORS }],
+  });
 
   if (!props.show) return null;
   else if (result.loading) return <div>loading ...</div>;
 
   const authors = result.data.allAuthors;
+
+  const handleOnSubmitBirthYear = (event) => {
+    event.preventDefault();
+
+    const name = event.target.name.value;
+    const born = Number(event.target.born.value);
+    event.target.name.value = "";
+    event.target.born.value = "";
+
+    editAuthor({ variables: { name, setBornTo: born } });
+  };
 
   return (
     <div>
@@ -28,6 +43,16 @@ const Authors = (props) => {
           ))}
         </tbody>
       </table>
+      <h1>Set Birthyear</h1>
+      <form onSubmit={handleOnSubmitBirthYear}>
+        <div>
+          name <input name="name" />
+        </div>
+        <div>
+          born <input name="born" type="number" />
+        </div>
+        <button type="submit">update author</button>
+      </form>
     </div>
   );
 };
