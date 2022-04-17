3906bf8f69b25aff6689ee431bb28453b3482f8f
mrgreen_pk <aamir.fui@gmail.com>
2022-04-17 12:15:36 +0500

    8.12 competed drt birthyear completed

--- library-frontend/src/components/Authors.js
+++ library-frontend/src/components/Authors.js
@@ -1,4 +1,5 @@
 import { gql, useMutation, useQuery } from "@apollo/client";
+import { useState } from "react";
 import { ALL_AUTHORS, EDIT_AUTHOR } from "../queries.js";
 
 const Authors = (props) => {
@@ -15,12 +16,10 @@ const Authors = (props) => {
 
   const handleOnSubmitBirthYear = (event) => {
     event.preventDefault();
-
-    const name = event.target.name.value;
+    const name = event.target.ddlName.value;
     const born = Number(event.target.born.value);
-    event.target.name.value = "";
+    event.target.ddlName.value = "";
     event.target.born.value = "";
-
     editAuthor({ variables: { name, setBornTo: born } });
   };
 
@@ -46,8 +45,16 @@ const Authors = (props) => {
       <h1>Set Birthyear</h1>
       <form onSubmit={handleOnSubmitBirthYear}>
         <div>
-          name <input name="name" />
+          name
+          <select name="ddlName">
+            {authors.map((author) => (
+              <option value={author.name}>{author.name}</option>
+            ))}
+          </select>
         </div>
+        {/* <div>
+          name <input name="name" />
+        </div> */}
         <div>
           born <input name="born" type="number" />
         </div>
